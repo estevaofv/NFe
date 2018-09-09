34aef4e2a1e6bb1528c1e672a0280d74e7a863de
jvmello <jvmello@inf.ufsm.br>
2018-09-08 21:43:49 -0300

    Atualizações no banco

--- portal-NFe/core/models.py
+++ portal-NFe/core/models.py
@@ -1,2 +1,22 @@
 from django.db import models
 
+
+class Nota(models.Model):
+    cod_nota = models.CharField(max_length=20)
+    chave_acesso = models.charField(max_length=50)
+    num_produtos = models.DecimalField(max_digits=5)
+    lista_produtos = models.DecimalField #Pensando em como adaptar pra pegar tudo da classe produto
+    #data_emissao?
+    #hora_emissao?
+
+
+class Produto(models.Model):
+    cod_prod = models.CharField(max_length=20)
+    descricao_prod = models.TextField
+    valor_un = models.FloatField
+    #Aqui tentei pegar só o que a gente REALMENTE precisa, o resto acho que dá pra fazer quebrando XML
+
+class Supermercado(models.Model): #Pode futuramente virar 'estabelecimento' tendo uma variável 'tipo'
+    nome_super = models.CharField()
+    #Endereço?
+    #Contato?
--- portal-NFe/notas/admin.py
+++ portal-NFe/notas/admin.py
@@ -1,2 +1,5 @@
 from django.contrib import admin
+from models import Nota, Produto, Supermercado
 
+modelos = [Nota, Produto, Supermercado]
+admin.site.register(modelos)
